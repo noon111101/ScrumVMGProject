0f06aefd98d3e65a78d543cc462adbe67e3a2106
noon111101 <trieu11112001@gmail.com>
2022-10-10 22:47:58 +0700

    feat : create structure

---
+++ src/main/java/com/vmg/scrum/entity/ERole.java
@@ -0,0 +1,7 @@
+package com.vmg.scrum.entity;
+
+public enum ERole {
+    ROLE_USER,
+    ROLE_MANAGE,
+    ROLE_ADMIN
+}
---
+++ src/main/java/com/vmg/scrum/entity/Role.java
@@ -0,0 +1,22 @@
+package com.vmg.scrum.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+public class Role extends BaseEntity {
+    @Enumerated(EnumType.STRING)
+    @Column(length = 20)
+    private ERole name;
+    @ManyToMany(mappedBy = "roles")
+    @JsonIgnore
+    private Set<User> users;
+}
