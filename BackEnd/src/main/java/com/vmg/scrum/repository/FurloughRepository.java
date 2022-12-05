d6a2d72fd52249de5c105a31deaeb7f57229c169
Nguyen Dinh Phu <phunguyen06072001@gmail.com>
2022-12-05 09:22:11 +0700

    Quản lý lễ

--- BackEnd/src/main/java/com/vmg/scrum/repository/FurloughRepository.java
+++ BackEnd/src/main/java/com/vmg/scrum/repository/FurloughRepository.java
@@ -2,9 +2,11 @@ package com.vmg.scrum.repository;
 
 import com.vmg.scrum.model.furlough.Furlough;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface FurloughRepository extends JpaRepository<Furlough,Long> {
     List<Furlough> findByYearAndUserId(Long year , Long userId);
 
