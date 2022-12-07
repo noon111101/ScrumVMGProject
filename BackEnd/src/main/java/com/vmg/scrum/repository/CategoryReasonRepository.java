d601482dc46292cd385e8f0a2b5ecad507d21036
Nguyen Dinh Phu <phunguyen06072001@gmail.com>
2022-12-07 09:58:04 +0700

    Quản lý đề xuất

--- BackEnd/src/main/java/com/vmg/scrum/repository/CategoryReasonRepository.java
+++ BackEnd/src/main/java/com/vmg/scrum/repository/CategoryReasonRepository.java
@@ -4,8 +4,11 @@ import com.vmg.scrum.model.request.CategoryReason;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CategoryReasonRepository extends JpaRepository<CategoryReason, Long> {
-
     CategoryReason findById(long id);
+
+    List<CategoryReason> getCategoryReasonByCatergoryRequest_Id(long id);
 }
