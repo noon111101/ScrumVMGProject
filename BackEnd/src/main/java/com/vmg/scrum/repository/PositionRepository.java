6a0ca8e4f05edd5c9a2882c78ebeb83429dc457b
noon111101 <trieu11112001@gmail.com>
2022-12-29 14:04:48 +0700

    fix :  system

--- BackEnd/src/main/java/com/vmg/scrum/repository/PositionRepository.java
+++ BackEnd/src/main/java/com/vmg/scrum/repository/PositionRepository.java
@@ -10,5 +10,7 @@ import java.util.List;
 public interface PositionRepository extends JpaRepository<Position, Long> {
     Position findById(long id);
 
+    Position findByName(String name);
+
     List<Position> findAll();
 }
